3f26591bde90bc2e4ff95ee9ea3da8373fd410b7
Michael <mmcintyre@ebsco.com>
2020-01-26 18:31:18 -0500

    completed exercise 93

--- week5-093.PersonExtended/src/Person.java
+++ week5-093.PersonExtended/src/Person.java
@@ -1,33 +1,54 @@
+
 import java.util.Calendar;
 
 public class Person {
+
     private String name;
     private MyDate birthday;
-    
-    public Person(String name, int pp, int kk, int vv) {
+
+    public Person(String name) {
         this.name = name;
-        this.birthday = new MyDate(pp, kk, vv);
+        this.birthday = this.getCurrentDate();
     }
-    
-    
+
+    public Person(String name, int day, int month, int year) {
+        this.name = name;
+        this.birthday = new MyDate(day, month, year);
+    }
+
+    public Person(String name, MyDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public MyDate getCurrentDate() {
+        int currentDay = Calendar.getInstance().get(Calendar.DATE);
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        MyDate currentDate = new MyDate(currentDay, currentMonth, currentYear);
+
+        return currentDate;
+    }
+
     public int age() {
-// calculate the age based on the birthday and the current day
-        // you get the current day as follows: 
-        // Calendar.getInstance().get(Calendar.DATE);
-        // Calendar.getInstance().get(Calendar.MONTH) + 1; // January is 0 so we add one
-        // Calendar.getInstance().get(Calendar.YEAR);
-        return 0;
+        MyDate currentDate = this.getCurrentDate();
+
+        return this.birthday.differenceInYears(currentDate);
+
     }
-    
+
     public boolean olderThan(Person compared) {
-        // compare the ages based on birthdays
-        return false;
+        if (this.age() == compared.age()) {
+            return this.birthday.getDaysIn365() > compared.birthday.getDaysIn365();
+        }
+        return this.age() > compared.age();
     }
-    
+
     public String getName() {
         return this.name;
     }
-    
+
     public String toString() {
         return this.name + ", born " + this.birthday;
     }
