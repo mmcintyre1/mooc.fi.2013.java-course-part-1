d05ff97406c60f5e7ae4d94a1f99ade555b4bcca
Michael <mmcintyre@ebsco.com>
2020-01-26 17:17:14 -0500

    completed exercise 91

--- week5-091.ExtendingMyDate/src/MyDate.java
+++ week5-091.ExtendingMyDate/src/MyDate.java
@@ -32,4 +32,31 @@ public class MyDate {
         return false;
     }
 
+    public void advance() {
+        this.day++;
+        if (this.day > 30) {
+            this.day = 1;
+            this.month++;
+            if (this.month > 12) {
+                this.year++;
+                this.month = 1;
+            }
+        }
+    }
+
+    public void advance(int numberOfDays) {
+        int i = 0;
+        while (i < numberOfDays) {
+            advance();
+            i++;
+        }
+    }
+
+    public MyDate afterNumberOfDays(int days) {
+
+        MyDate newMyDate = new MyDate(this.day, this.month, this.year);
+        newMyDate.advance(days);
+
+        return newMyDate;
+    }
 }
