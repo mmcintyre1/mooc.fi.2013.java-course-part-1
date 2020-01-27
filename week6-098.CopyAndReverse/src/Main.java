bc9a7b1d715f6f8c45cf26b46c5a6e33ca2c89c9
Michael <mmcintyre@ebsco.com>
2020-01-26 23:06:12 -0500

    completed exercise 98

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,36 @@
 
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
+        int[] original = {1, 2, 3, 4};
+        int[] reverse = reverseCopy(original);
+
+        // print both
+        System.out.println("original: " + Arrays.toString(original));
+        System.out.println("reversed: " + Arrays.toString(reverse));
     }
-    
+
+    public static int[] copy(int[] array) {
+        int[] copiedArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            copiedArray[i] = array[i];
+        }
+
+        return copiedArray;
+    }
+
+    public static int[] reverseCopy(int[] array) {
+        int[] reverseCopiedArray = new int[array.length];
+
+        for (int i = array.length; i > 0; i--) {
+            System.out.println(i);
+            reverseCopiedArray[array.length - i] = array[i - 1];
+        }
+
+        return reverseCopiedArray;
+    }
+
 }
