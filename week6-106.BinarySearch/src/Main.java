52927270c96d6531bf1436820099bd32cd39dbd5
Michael <mmcintyre@ebsco.com>
2020-02-01 17:04:26 -0500

    completed exercise 106

--- week6-106.BinarySearch/src/Main.java
+++ week6-106.BinarySearch/src/Main.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Test your program here
-        int[] taulukko = {-3, 2, 3, 4, 7, 8, 12};
-        Scanner lukija = new Scanner(System.in);
+        // Here you can test binary search
+        int[] array = { -3, 2, 3, 4, 7, 8, 12 };
+        Scanner reader = new Scanner(System.in);
 
-        System.out.print("Numbers in the array " + Arrays.toString(taulukko));
+        System.out.print("Values of the array: " + Arrays.toString(array));
         System.out.println();
 
         System.out.print("Enter searched number: ");
-        String etsittavaLuku = lukija.nextLine();
+        String searchedValue = reader.nextLine();
         System.out.println();
 
-        boolean tulos = BinarySearch.search(taulukko, Integer.parseInt(etsittavaLuku));
+        boolean result = BinarySearch.search(array, Integer.parseInt(searchedValue));
 
-        // Print here the result
+        System.out.println(String.format("%s in the array: %b", searchedValue, result));
     }
 }
